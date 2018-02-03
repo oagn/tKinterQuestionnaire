a74d9202ffa3bce81135e353d03a8bda75be8421
oagn <agnethe.s.olsen@gmail.com>
2018-02-03 17:04:21 +0000

    Added new retrieveResponse method and initialised counters

--- DisplayResults.py
+++ DisplayResults.py
@@ -9,6 +9,19 @@ class DisplayResults(Frame):
         Frame.__init__(self, master)
         self.pack()
 
+    def retrieveResponse(self):
+        # retrieve responses from database
+        countAll = 0
+        sumQ1SE = 0.0
+        sumQ2SE = 0.0
+        sumQ3SE = 0.0
+        sumP1Joints = 0
+        sumP2Joints = 0
+        sumP3Joints = 0
+        sumP4Joints = 0
+        sumP5Joints = 0
+        sumP6Joints = 0
+
 
 # Main
 root = Tk()
